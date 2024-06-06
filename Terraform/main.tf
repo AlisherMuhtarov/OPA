3497085b2d82859a2254c08022d1073298996d6d
alisher muhtarov <therealkimbladi@gmail.com>
2024-06-06 10:49:23 -0500

    test

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,7 @@
-provider "aws" {
-  region = "us-east-1"
+resource "aws_instance" "uncompliant" {
+  ami           = "ami-00beae93a2d981137"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-098ab2c379f487d8e"
+  key_name = "laptop_key"
+  security_groups = ["uncompliant"]
 }
